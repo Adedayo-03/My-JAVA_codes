294e20324778efe442fa95b90ab4bf5773298dae
Adedayo-03 <princeadedayo03@gmail.com>
2022-10-01 03:28:57 +0100

    Tells my name and welcomes me

---
+++ My_Name/main.java
@@ -0,0 +1,9 @@
+public class main {
+	static void Name (String fname, String lname) {
+		String Fname = fname + " " + lname;
+		System.out.println("Welcome" + " " + Fname.toUpperCase());
+	}
+	public static void main (String[] args) {
+		Name("adedayo", "adejare");
+	}
+}
