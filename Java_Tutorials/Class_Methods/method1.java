8abeac77092cfadcd4a1d4d13b51ae5c7092e1de
Adedayo-03 <princeadedayo03@gmail.com>
2022-10-08 01:37:45 +0100

    calling of a class into another

---
+++ Java_Tutorials/Class_Methods/method1.java
@@ -0,0 +1,15 @@
+package Class_Methods;
+
+public class method1 {
+	public void Indicator () {
+		System.out.println("This car is moving as fast as it can");
+	}
+	public void Speed (int a) {
+		System.out.println("The max is:" + " " + a);
+	}
+	// public static void main(String[] args) {
+	// 	method1 myObj = new method1();
+	// 	myObj.Indicator();
+	// 	myObj.Speed(380);
+	// }
+}
