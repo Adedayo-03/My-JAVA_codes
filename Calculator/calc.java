4e22475142b7364e760a1f3af271a18db178e547
D'Prince <princeadedayo03@gmail.com>
2022-10-31 19:45:22 +0100

    just pushing

--- Calculator/calc.java
+++ Calculator/calc.java
@@ -1,50 +1,54 @@
-package Interface;
+package Calculator;
 
 import java.util.Scanner;
 
-interface Calculator {
+interface Calculator 
+{
 void add(double x, double y);
 void subtract(double x, double y);
 void divide(double x, double y);
-void multiply(double x, double y);
-	
+void multiply(double x, double y);	
 }
 
-class math_func implements Calculator {
-	public void add(double x, double y) {
+class math_func implements Calculator 
+{
+	public void add(double x, double y) 
+	{
 		System.out.println(x + y);
 	}
-	public void subtract(double x, double y) {
+	public void subtract(double x, double y) 
+	{
 		System.out.println(x - y);
 	}
-	public void divide(double x, double y) {
+	public void divide(double x, double y) 
+	{
 		System.out.println(x / y);
 	}
-	public void multiply(double x, double y) {
+	public void multiply(double x, double y) 
+	{
 		System.out.println(x * y);
 	} 
 }
 
-class main {
-
-	static void simple_calc() {
-		int a, b;
-
+class calc 
+{
+	static void simple_calc() 
+	{
 		Scanner fig = new Scanner(System.in);
-		
+		int a, b;
 		a = fig.nextInt();
 		b = fig.nextInt();
-
 		Calculator myObj = new math_func();
+
 		Scanner request = new Scanner(System.in);
-		
 		System.out.print("Hello!"+" ");
 		System.out.println("Do you want to calculate? Yes / No (Y / N):");
-		
 		String pick = request.nextLine();
 
-		if (pick.equals("Y")) {
-			for (int i = 0; i <= 1; i++) {
+		if (pick.equals("Y")) 
+		{
+			for (int i = 0; i <= 1; i++) 
+			{
 				System.out.println("Which mathimatical operators would you like to use?");
 				System.out.println("If you want to ADD choose : A");
 				System.out.println("If you want to SUBTRACT choose : B");
@@ -53,28 +57,38 @@ class main {
 				
 				String option = request.nextLine();
 
-				if (option.equals("A")) {
+				if (option.equals("A")) 
+				{
 					myObj.add(a, b);
 				}
-				else if (option.equals("B")) {
+				else if (option.equals("B")) 
+				{
 					myObj.divide(a, b);
 				} 
-				else if (option.equals("C")) {
+				else if (option.equals("C")) 
+				{
 					myObj.multiply(a, b);
 				}
-				else if (option.equals("D")) {
+				else if (option.equals("D")) 
+				{
 					myObj.subtract(a, b);
 				}
-				else {
+				else 
+				{
 					System.out.println("Please pick one of the above");
 				}
 			}
 		}
-		else {
+		else 
+		{
 			System.out.println("Maybe next time I could be of help :(");
 		}
 	}
-	public static void main(String [] args) {
+	/**
+	 * @param args
+	 */
+	public static void main (String [] args) 
+	{
 		simple_calc();
 	}
 }
